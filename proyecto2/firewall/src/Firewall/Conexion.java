a9e7486e1a25d8444eb65b9081e957ff15ae5fd7
Daniel Hamilton Smith <danihas@live.com>
2018-11-19 00:23:43 -0500

    correcciones puestas a obtenerTablaDirecciones, para evitar NullPointer exception, lo devolvi a que escuchara por todos los dispositivos. Es mas general

--- proyecto2/firewall/src/Firewall/Conexion.java
+++ proyecto2/firewall/src/Firewall/Conexion.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import static java.util.Collections.list;
 import java.util.List;
@@ -16,11 +18,12 @@ import jpcap.JpcapCaptor;
 import jpcap.JpcapSender;
 import jpcap.NetworkInterface;
 import jpcap.packet.EthernetPacket;
+import jpcap.packet.IPPacket;
 import jpcap.packet.Packet;
 
 public class Conexion implements Runnable 
 {
-    String[] nombreTabla = new String[2];
+    String[] nombreTabla;
     String[][] direccionIP = new String [2][20];
     Tabla[] tabla = new Tabla[2];
     // recepcion 
@@ -39,17 +42,27 @@ public class Conexion implements Runnable
     private final int cantHilos;
     private Semaphore sem;
     
+    /*
     public void startTask() {
-        hilo = new Thread(this);
-            hilo.start();
+        
         // se llama a los hilos 
-        /*for(int a = 0 ; a < cantHilos ; a ++) 
+        //for(int a = 0 ; a < cantHilos ; a ++) 
         {
-            
+            hilo = new Thread(this);
+            hilo.start();
         }
-        */
-    }
+        
+    }*/
     
+    public void startTask() {
+        
+        // se llama a los hilos 
+        for(int a = 0 ; a < cantHilos ; a ++) 
+        {
+            hilo = new Thread(this);
+            hilo.start();
+        }
+    }
     
     
     public Conexion() {
@@ -60,6 +73,10 @@ public class Conexion implements Runnable
         dispositivos = JpcapCaptor.getDeviceList();
         cantHilos = dispositivos.length;
         sem = new Semaphore(cantHilos,true);
+        
+        nombreTabla = new String[cantHilos];
+        
+        obtenerTablaDirecciones();
     }
 
     public void pause() {
@@ -77,10 +94,11 @@ public class Conexion implements Runnable
         isRunning = false;
     }
     
-    /*
     @Override
     public void run() {
         
+        JpcapCaptor captor = null ;
+        JpcapSender sender = null;
         try {
             sem.acquire();
         } catch (InterruptedException ex) {
@@ -89,47 +107,71 @@ public class Conexion implements Runnable
         int pos = numHilos;
         numHilos++;
         sem.release();
-    
+        NetworkInterface device = dispositivos[pos];
+        
         try {
+            captor = JpcapCaptor.openDevice(device,2000,false,5000);
+            sender = captor.getJpcapSenderInstance();
+            
+            
+            agregarFiltro(captor);
+            
+            try {
             NetworkInterface dispositivo;
 
             dispositivo = dispositivos[pos];
 
             JpcapCaptor capturador = JpcapCaptor.openDevice(dispositivo, 65535, modoDeCaptura, 20);
 
-            while (isRunning) {
+                while (isRunning) 
+                {
 
-                synchronized (pauseLock) {
-                    if (!isRunning) {
-                        break;
+                    synchronized (pauseLock) 
+                    {
+                        if (!isRunning) 
+                        {
+                            break;
+                        }
+                        if (paused) 
+                        {
+                            try {
+                                pauseLock.wait();
+                            } 
+                            catch (InterruptedException ex) 
+                            {
+                                break;
+                            }
+                            if (!isRunning) 
+                            {
+                                break;
+                            }
+                        }
                     }
-                    if (paused) {
-                        try {
-                            pauseLock.wait();
-                        } catch (InterruptedException ex) {
-                            break;
-                        }
-                        if (!isRunning) {
-                            break;
-                        }
+
+                    Packet pak = captor.getPacket();
+                    
+                    if(pak!=null)
+                    {
+                        System.out.println(pak);
+                    
+                        //pak = modificarPaquete(pak);
+
+                        sender.sendPacket(pak);
                     }
                 }
+                capturador.close();
 
-                ReceptorPaquetes receptor = new ReceptorPaquetes();
-                capturador.processPacket(1, receptor);
+            } 
+            catch (IOException ex) 
+            {
 
             }
-            capturador.close();
-
+            
         } catch (IOException ex) {
-
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
     }
-    
-    /*/
-    
+    /* LLL----
     @Override
     public void run() {
         
@@ -218,7 +260,12 @@ public class Conexion implements Runnable
             Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    */
+    
+    void imp(String str)
+    {
+        System.out.print(str);
+    }
     
     private void obtenerTablaDirecciones(){
         int contador=0, con=0, no=0;
@@ -248,6 +295,7 @@ public class Conexion implements Runnable
                 if(aux.contains("Interfaz")){
                     no = con;  
                     nombreTabla[contador] = aux;
+                    imp("pp ---- "+aux+"\n");
                     aux = br.readLine();
                     contador++;
                     con=0;
@@ -274,25 +322,34 @@ public class Conexion implements Runnable
      
        
         for(int i=0; i<nombreTabla.length; i++){
-            datosDireccion = nombreTabla[i].split(" ");
-            for(int j=0; j<direccionIP[i].length; j++){
-                if(direccionIP[i][j]!=null && j!=no-1){
-                    StringTokenizer tokens = new StringTokenizer(direccionIP[i][j]);
-                    datosIP.add(tokens.nextToken());
-                    datosMAC.add(tokens.nextToken());
-                } 
+            if(nombreTabla[i] != null)
+            {
+                imp(nombreTabla[i]+"\n");
+                datosDireccion = nombreTabla[i].split(" ");
+                for(int j=0; j<direccionIP[i].length; j++){
+                    if(direccionIP[i][j]!=null && j!=no-1){
+                        StringTokenizer tokens = new StringTokenizer(direccionIP[i][j]);
+                        datosIP.add(tokens.nextToken());
+                        datosMAC.add(tokens.nextToken());
+                    } 
+                }
+                tabla[i] = new Tabla(datosDireccion[1], datosIP, datosMAC,datosIP.size());
+                datosIP = new ArrayList<String>();
+                datosMAC = new ArrayList<String>();
             }
-            tabla[i] = new Tabla(datosDireccion[1], datosIP, datosMAC,datosIP.size());
-            datosIP = new ArrayList<String>();
-            datosMAC = new ArrayList<String>();
+            
         }
         
         System.out.println("---------------------------------------------");
         for(int i=0; i<tabla.length; i++){
-            System.out.println("Interfaz "+tabla[i].direccion);
-            for(int j=0; j<tabla[i].tam; j++){
-                 System.out.println("ip MAC  "+tabla[i].direccionesIP.get(j)+ " |-| "+tabla[i].direccionesMAC.get(j));
+            if (tabla[i] != null)
+            {
+                System.out.println("Interfaz "+tabla[i].direccion);
+                for(int j=0; j<tabla[i].tam; j++){
+                     System.out.println("ip MAC  "+tabla[i].direccionesIP.get(j)+ " |-| "+tabla[i].direccionesMAC.get(j));
+                }
             }
+            
         }
         System.out.println("---------------------------------------------");
         
@@ -300,13 +357,23 @@ public class Conexion implements Runnable
     
     private Packet modificarPaquete(Packet pak) {
          
-      
+        /*
+         IPPacket ip = new IPPacket();
+         ip.
+         // juejuejue, solo para probar que sucede un reenvio
+         InetAddress ia = ip.src_ip;
+         ip.src_ip = ip.dst_ip;
+         ip.dst_ip = ia;
+         
+         /*/
+         
          EthernetPacket ether=new EthernetPacket();
          ether.frametype=EthernetPacket.ETHERTYPE_IP;
          ether.src_mac=new byte[]{(byte)108,(byte)59,(byte)229,(byte)140,(byte)105,(byte)74};
          ether.dst_mac=new byte[]{(byte)112,(byte)28,(byte)231,(byte)235,(byte)32,(byte)79};
+         
          pak.datalink=ether;
-        
+         //*/
         return pak;
     }
 
